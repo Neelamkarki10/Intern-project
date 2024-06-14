ec6ec16474d13c1d4f58bbe4235887ee6372bb45
Neelam Karki <karkineelam200@gmail.com>
2024-06-14 18:54:51 +0545

    some changes

---
+++ res/js/new-attendance.js
@@ -0,0 +1,6 @@
+function loadStudents() {
+  document.getElementsByTagName("section")[0].style.display = "none";
+  document.getElementsByTagName("main")[0].style.paddingTop = "2rem";
+
+  document.getElementById("form").style.display = "block";
+}
