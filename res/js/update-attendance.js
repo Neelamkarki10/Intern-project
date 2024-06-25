74c03b43aa53677538686b42aa3ecb97fce11ac5
Bipul Karki <bipulkarki6@gmail.com>
2024-06-25 21:58:31 +0545

    webpage for update attendance with select logic

---
+++ res/js/update-attendance.js
@@ -0,0 +1,8 @@
+function loadStudents() {
+    document.getElementsByTagName("section")[0].style.display = "none";
+    document.getElementsByTagName("main")[0].style.paddingTop = "7rem";
+  
+    document.getElementById("form").style.display = "block";
+    document.getElementById("form").style.padding = "0 3rem 0 3rem";
+  }
+  
